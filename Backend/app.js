935f74f4216b3a3cd939a740334c36cfdaa2f209
mahos18 <sohamlohote@gmail.com>
2026-04-30 00:57:24 +0530

    cors-maha-update

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,31 @@ const friendRoutes = require('./routes/friendRouter');
 const postRoutes = require('./routes/postRouter');
 
 const PORT = process.env.PORT || 8080;
-app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://skill-sync-frontend-neon.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+const corsOptions = {
+  origin: "https://skill-sync-frontend-neon.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+// 👇 VERY IMPORTANT for preflight
+app.options('*', cors(corsOptions));
+
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.get('/', (req, res) => {
